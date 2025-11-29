93fe19cbd9e502733229b4c2f4c50501a4188f83
Jonibek <chartakiy@gmail.com>
2025-11-29 16:17:59 +0500

    everything done

--- configapp/urls.py
+++ configapp/urls.py
@@ -1,24 +1,41 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name = "configapp"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
 
-    path("otm/", views.otm_list, name="otm_list"),
-    path("otm/add/", views.otm_add, name="otm_create"),
-    path("otm/edit/<int:pk>/", views.otm_edit, name="otm_edit"),
-    path("otm/delete/<int:pk>/", views.otm_delete, name="otm_delete"),
+    # path("otm/", views.otm_list, name="otm_list"),
+    # path("otm/add/", views.otm_add, name="otm_create"),
+    # path("otm/edit/<int:pk>/", views.otm_edit, name="otm_edit"),
+    # path("otm/delete/<int:pk>/", views.otm_delete, name="otm_delete"),
 
-    path("groups/", views.group_list, name="group_list"),
-    path("groups/add/", views.group_add, name="group_create"),
-    path("groups/edit/<int:pk>/", views.group_edit, name="group_edit"),
-    path("groups/delete/<int:pk>/", views.group_delete, name="group_delete"),
+    # path("groups/", views.group_list, name="group_list"),
+    # path("groups/add/", views.group_add, name="group_create"),
+    # path("groups/edit/<int:pk>/", views.group_edit, name="group_edit"),
+    # path("groups/delete/<int:pk>/", views.group_delete, name="group_delete"),
 
-    path("students/", views.student_list, name="student_list"),
-    path("students/add/", views.student_add, name="student_create"),
-    path("students/edit/<int:pk>/", views.student_edit, name="student_edit"),
-    path("students/delete/<int:pk>/", views.student_delete, name="student_delete"),
-    path("students/<int:pk>/", views.student_detail, name="student_detail"),
+    # path("students/", views.student_list, name="student_list"),
+    # path("students/add/", views.student_add, name="student_create"),
+    # path("students/edit/<int:pk>/", views.student_edit, name="student_edit"),
+    # path("students/delete/<int:pk>/", views.student_delete, name="student_delete"),
+    # path("students/<int:pk>/", views.student_detail, name="student_detail"),
+    path("", IndexView.as_view(), name="index"),
+
+    path("otm/", OTMListView.as_view(), name="otm_list"),
+    path("otm/add/", OTMCreateView.as_view(), name="otm_create"),
+    path("otm/edit/<int:pk>/", OTMUpdateView.as_view(), name="otm_edit"),
+    path("otm/delete/<int:pk>/", OTMDeleteView.as_view(), name="otm_delete"),
+
+    path("groups/", GroupListView.as_view(), name="group_list"),
+    path("groups/add/", GroupCreateView.as_view(), name="group_create"),
+    path("groups/edit/<int:pk>/", GroupUpdateView.as_view(), name="group_edit"),
+    path("groups/delete/<int:pk>/", GroupDeleteView.as_view(), name="group_delete"),
+
+    path("students/", StudentListView.as_view(), name="student_list"),
+    path("students/add/", StudentCreateView.as_view(), name="student_create"),
+    path("students/edit/<int:pk>/", StudentUpdateView.as_view(), name="student_edit"),
+    path("students/delete/<int:pk>/", StudentDeleteView.as_view(), name="student_delete"),
+    path("students/<int:pk>/", StudentDetailView.as_view(), name="student_detail"),
 ]
--- configapp/views.py
+++ configapp/views.py
@@ -1,215 +1,197 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from .models import OTM, Group, Student
 from .forms import OTMForm, GroupForm, StudentForm
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, DetailView
 
-def index(request):
-    return render(request, "index.html")
+# def index(request):
+#     return render(request, "index.html")
+
+class IndexView(TemplateView):
+    template_name = "index.html"
 
 
 # ----------------------- OTM -----------------------
 
-def otm_list(request):
-    otms = OTM.objects.all()
-    return render(request, "otm_list.html", {"otms": otms})
+# def otm_list(request):
+#     otms = OTM.objects.all()
+#     return render(request, "otm_list.html", {"otms": otms})
 
+class OTMListView(ListView):
+    model = OTM
+    template_name = "otm_list.html"
+    context_object_name = "otms"
 
 # def otm_add(request):
 #     if request.method == "POST":
-#         OTM.objects.create(
-#             otm_name=request.POST["name"],
-#             otm_email=request.POST["email"],
-#             otm_phone_number=request.POST["phone"],
-#             otm_address=request.POST["address"],
-#         )
-#         return redirect("configapp:otm_list")
-#     return render(request, "otm_form.html")
+#         form = OTMForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:otm_list")
+#     else:
+#         form = OTMForm()
 
-def otm_add(request):
-    if request.method == "POST":
-        form = OTMForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:otm_list")
-    else:
-        form = OTMForm()
+#     return render(request, "otm_form.html", {"form": form})
 
-    return render(request, "otm_form.html", {"form": form})
-
+class OTMCreateView(CreateView):
+    model = OTM
+    form_class = OTMForm
+    template_name = "otm_form.html"
+    success_url = reverse_lazy("configapp:otm_list")
 
 # def otm_edit(request, pk):
 #     otm = get_object_or_404(OTM, pk=pk)
 
 #     if request.method == "POST":
-#         otm.otm_name = request.POST["name"]
-#         otm.otm_email = request.POST["email"]
-#         otm.otm_phone_number = request.POST["phone"]
-#         otm.otm_address = request.POST["address"]
-#         otm.save()
-#         return redirect("configapp:otm_list")
+#         form = OTMForm(request.POST, instance=otm)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:otm_list")
+#     else:
+#         form = OTMForm(instance=otm)
 
-#     return render(request, "otm_form.html", {"otm": otm})
+#     return render(request, "otm_form.html", {"form": form})
 
-def otm_edit(request, pk):
-    otm = get_object_or_404(OTM, pk=pk)
+class OTMUpdateView(UpdateView):
+    model = OTM
+    form_class = OTMForm
+    template_name = "otm_form.html"
+    success_url = reverse_lazy("configapp:otm_list")
 
-    if request.method == "POST":
-        form = OTMForm(request.POST, instance=otm)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:otm_list")
-    else:
-        form = OTMForm(instance=otm)
+# def otm_delete(request, pk):
+#     otm = get_object_or_404(OTM, pk=pk)
+#     otm.delete()
+#     return redirect("configapp:otm_list")
 
-    return render(request, "otm_form.html", {"form": form})
-
-def otm_delete(request, pk):
-    otm = get_object_or_404(OTM, pk=pk)
-    otm.delete()
-    return redirect("configapp:otm_list")
+class OTMDeleteView(DeleteView):
+    model = OTM
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("configapp:otm_list")
 
 
 # ----------------------- GROUP -----------------------
 
-def group_list(request):
-    groups = Group.objects.all()
-    return render(request, "group_list.html", {"groups": groups})
+# def group_list(request):
+#     groups = Group.objects.all()
+#     return render(request, "group_list.html", {"groups": groups})
 
+class GroupListView(ListView):
+    model = Group
+    template_name = "group_list.html"
+    context_object_name = "groups"
 
 # def group_add(request):
-#     otms = OTM.objects.all()
-
 #     if request.method == "POST":
-#         Group.objects.create(
-#             group_name=request.POST["name"],
-#             group_field=request.POST["field"],
-#             otm_id=request.POST["otm"]
-#         )
-#         return redirect("configapp:group_list")
+#         form = GroupForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:group_list")
+#     else:
+#         form = GroupForm()
 
-#     return render(request, "group_form.html", {"otms": otms})
+#     return render(request, "group_form.html", {"form": form})
 
-def group_add(request):
-    if request.method == "POST":
-        form = GroupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:group_list")
-    else:
-        form = GroupForm()
-
-    return render(request, "group_form.html", {"form": form})
+class GroupCreateView(CreateView):
+    model = Group
+    form_class = GroupForm
+    template_name = "group_form.html"
+    success_url = reverse_lazy("configapp:group_list")
 
 
 # def group_edit(request, pk):
 #     group = get_object_or_404(Group, pk=pk)
-#     otms = OTM.objects.all()
 
 #     if request.method == "POST":
-#         group.group_name = request.POST["name"]
-#         group.group_field = request.POST["field"]
-#         group.otm_id = request.POST["otm"]
-#         group.save()
-#         return redirect("configapp:group_list")
+#         form = GroupForm(request.POST, instance=group)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:group_list")
+#     else:
+#         form = GroupForm(instance=group)
 
-#     return render(request, "group_form.html", {"group": group, "otms": otms})
-
-def group_edit(request, pk):
-    group = get_object_or_404(Group, pk=pk)
-
-    if request.method == "POST":
-        form = GroupForm(request.POST, instance=group)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:group_list")
-    else:
-        form = GroupForm(instance=group)
-
-    return render(request, "group_form.html", {"form": form})
+#     return render(request, "group_form.html", {"form": form})
+class GroupUpdateView(UpdateView):
+    model = Group
+    form_class = GroupForm
+    template_name = "group_form.html"
+    success_url = reverse_lazy("configapp:group_list")
 
 
-def group_delete(request, pk):
-    group = get_object_or_404(Group, pk=pk)
-    group.delete()
-    return redirect("configapp:group_list")
+# def group_delete(request, pk):
+#     group = get_object_or_404(Group, pk=pk)
+#     group.delete()
+#     return redirect("configapp:group_list")
+
+class GroupDeleteView(DeleteView):
+    model = Group
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("configapp:group_list")
 
 
 # ----------------------- STUDENT -----------------------
 
-def student_list(request):
-    students = Student.objects.all()
-    return render(request, "student_list.html", {"students": students})
+# def student_list(request):
+#     students = Student.objects.all()
+#     return render(request, "student_list.html", {"students": students})
 
+class StudentListView(ListView):
+    model = Student
+    template_name = "student_list.html"
+    context_object_name = "students"
 
 # def student_add(request):
-#     groups = Group.objects.all()
-
 #     if request.method == "POST":
-#         Student.objects.create(
-#             student_f_name=request.POST["fname"],
-#             student_l_name=request.POST["lname"],
-#             student_email=request.POST["email"],
-#             student_phone_number=request.POST["phone"],
-#             student_birth_year=request.POST["birth"],
-#             student_image=request.FILES.get("image"),
-#             group_id=request.POST["group"]
-#         )
-#         return redirect("configapp:student_list")
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:student_list")
+#     else:
+#         form = StudentForm()
 
-#     return render(request, "student_form.html", {"groups": groups})
+#     return render(request, "student_form.html", {"form": form})
 
-def student_add(request):
-    if request.method == "POST":
-        form = StudentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:student_list")
-    else:
-        form = StudentForm()
-
-    return render(request, "student_form.html", {"form": form})
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "student_form.html"
+    success_url = reverse_lazy("configapp:student_list")
 
 
 # def student_edit(request, pk):
 #     student = get_object_or_404(Student, pk=pk)
-#     groups = Group.objects.all()
 
 #     if request.method == "POST":
-#         student.student_f_name = request.POST["fname"]
-#         student.student_l_name = request.POST["lname"]
-#         student.student_email = request.POST["email"]
-#         student.student_phone_number = request.POST["phone"]
-#         student.student_birth_year = request.POST["birth"]
+#         form = StudentForm(request.POST, request.FILES, instance=student)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("configapp:student_list")
+#     else:
+#         form = StudentForm(instance=student)
 
-#         if request.FILES.get("image"):
-#             student.student_image = request.FILES["image"]
+#     return render(request, "student_form.html", {"form": form})
 
-#         student.group_id = request.POST["group"]
-#         student.save()
-
-#         return redirect("configapp:student_list")
-
-#     return render(request, "student_form.html", {"student": student, "groups": groups})
-
-def student_edit(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-
-    if request.method == "POST":
-        form = StudentForm(request.POST, request.FILES, instance=student)
-        if form.is_valid():
-            form.save()
-            return redirect("configapp:student_list")
-    else:
-        form = StudentForm(instance=student)
-
-    return render(request, "student_form.html", {"form": form})
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "student_form.html"
+    success_url = reverse_lazy("configapp:student_list")
 
 
-def student_delete(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    student.delete()
-    return redirect("configapp:student_list")
+# def student_delete(request, pk):
+#     student = get_object_or_404(Student, pk=pk)
+#     student.delete()
+#     return redirect("configapp:student_list")
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("configapp:student_list")
 
 
-def student_detail(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    return render(request, "student_detail.html", {"student": student})
+# def student_detail(request, pk):
+#     student = get_object_or_404(Student, pk=pk)
+#     return render(request, "student_detail.html", {"student": student})
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = "student_detail.html"
